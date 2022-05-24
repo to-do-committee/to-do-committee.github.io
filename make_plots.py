24cea5ad26ff40df031ec704226d154e1c87ab99
tabrom <tabrom@web.de>
2022-05-24 18:35:58 +0200

    toilet dat week 5

--- make_plots.py
+++ make_plots.py
@@ -1,6 +1,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+def make_plots(results1, results2, xlabels, cat1, cat2, output_file_name): 
+    X = np.arange(len(results1))
+    fig = plt.figure(dpi=100)
+    ax = fig.add_subplot(111)
+    ax.bar(X, results1, color = 'black', width = 0.25,label=cat1 ) 
+    ax.bar(X + 0.25, results2, color = 'orangered', width = 0.25,label=cat2)
+    ax.set_facecolor("silver")
+
+    xticks = [x+0.25 if results1[i]==0 else x if results2[i]==0 else x+0.125 for i, x in enumerate(X)] # checks whether any of the values is zero to center x-ticks under bar(s)
+    ax.set_xticks(xticks)
+    ax.set_xticklabels(xlabels,rotation=45, ha='right', rotation_mode='anchor')
+    ax.tick_params(axis='x', colors='grey') 
+    ax.spines['bottom'].set_color('grey') 
+
+    yticks = np.arange(0,max(results1+results2)+5,5)
+    ax.set_yticks(yticks)
+    ax.tick_params(axis='y', colors='grey')  #setting up Y-axis tick color to black
+    ax.spines['left'].set_color('grey')        # setting up Y-axis tick color to grey
+
+    plt.legend(prop={'size': 9})
+    plt.tight_layout() # make labels fit on figure
+
+    plt.rcParams['axes.facecolor']='silver'
+    plt.rcParams['savefig.facecolor']='#171819' # matching fig backgroudn with website colour 
+    plt.savefig(f'{output_file_name}.png', dpi=500, transparent=False)
+
 # X = np.arange(5)
 # fig = plt.figure(dpi=100)
 # ax = fig.add_axes([0,0,1,1])
@@ -42,26 +69,29 @@ import matplotlib.pyplot as plt
 
 # week 4
 
-X = np.arange(5)
-fig = plt.figure(dpi=100)
-# ax = fig.add_axes([0,0,1,1])
-ax = fig.add_subplot(111)
-ax.bar(X, [30,4,31,2,0], color = 'black', width = 0.25,label='Travel time <30min' ) # ,tick_label=['Vegan', 'Between Vegan and Vegetarian', 'Vegetarian', 'Flexiterian', 'Omnivore']
-ax.bar(X + 0.25, [0,0,0,9,4], color = 'orangered', width = 0.25,label='Travel time >30min')
-ax.set_facecolor("silver")
-ax.set_xticks(X)
-ax.set_xticklabels(['Walking', 'Cycling/Walking', 'The Dutch way (cycling)', "Public transport", 'Other'],rotation=45, ha='right', rotation_mode='anchor')
-ax.tick_params(axis='x', colors='grey') 
-ax.spines['bottom'].set_color('grey') 
+# X = np.arange(5)
+# fig = plt.figure(dpi=100)
+# # ax = fig.add_axes([0,0,1,1])
+# ax = fig.add_subplot(111)
+# ax.bar(X, [30,4,31,2,0], color = 'black', width = 0.25,label='Travel time <30min' ) # ,tick_label=['Vegan', 'Between Vegan and Vegetarian', 'Vegetarian', 'Flexiterian', 'Omnivore']
+# ax.bar(X + 0.25, [0,0,0,9,4], color = 'orangered', width = 0.25,label='Travel time >30min')
+# ax.set_facecolor("silver")
+# ax.set_xticks(X)
+# ax.set_xticklabels(['Walking', 'Cycling/Walking', 'The Dutch way (cycling)', "Public transport", 'Other'],rotation=45, ha='right', rotation_mode='anchor')
+# ax.tick_params(axis='x', colors='grey') 
+# ax.spines['bottom'].set_color('grey') 
 
-ax.set_yticks([0,5,10,15,20, 25])
-ax.tick_params(axis='y', colors='grey')  #setting up Y-axis tick color to black
-ax.spines['left'].set_color('grey')        # setting up Y-axis tick color to grey
+# ax.set_yticks([0,5,10,15,20, 25])
+# ax.tick_params(axis='y', colors='grey')  #setting up Y-axis tick color to black
+# ax.spines['left'].set_color('grey')        # setting up Y-axis tick color to grey
 
-plt.legend(prop={'size': 9})
-plt.tight_layout() # make labels fit on figure
+# plt.legend(prop={'size': 9})
+# plt.tight_layout() # make labels fit on figure
 
-plt.rcParams['axes.facecolor']='silver'
-plt.rcParams['savefig.facecolor']='#171819'
-plt.savefig('results_week4.png', dpi=500, transparent=False)
+# plt.rcParams['axes.facecolor']='silver'
+# plt.rcParams['savefig.facecolor']='#171819'
+# plt.savefig('results_week4.png', dpi=500, transparent=False)
 
+# week 5
+make_plots(results1=[29,5,35,4,9], results2=[12,0,14,17,8], xlabels=['Home Office', 'Both', 'Beurs', 'Tresoar', 'Dbieb'], 
+    cat1='Basically always there', cat2='Only there when shit hits the fan', output_file_name='results_week5')
