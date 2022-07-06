894ca450d3657ada8677914d40914e3e6238ffda
tabrom <tabrom@web.de>
2022-07-06 18:41:06 +0200

    streamline new posts + week6

---
+++ toilet_data/new_poster.py
@@ -0,0 +1,35 @@
+import os
+
+def new_post(week_number,  picture, poster_text_file='new_poster_text.txt'):
+    """
+    enter week number as number (above 12) or written with a captital letter
+
+    enter the file name for the picture (and path if not in the same dir)
+    
+    enter file name of file with the corresponding text, only in one line, 
+    or delete the first line of the existing file and write it there
+    """
+
+    end = "    <!-- toilet data end -->"
+
+    with open(poster_text_file, 'r') as t:
+        poster_text=t.read()
+
+    with open("poster_template.txt", 'rt') as t:
+        template = t.read()
+
+        template = template.replace('WEEK_NUMBER', str(week_number))
+        template = template.replace('PLACEHOLDER_POSTER_TEXT', poster_text)
+        template = template.replace('PLACEHOLDER_PIC', picture)
+
+    with open("index.html", "rt") as fin:
+        with open("out.html", "wt") as fout:
+            for line in fin:
+                fout.write(line.replace(end, template))
+    
+    os.remove("index.html")
+    os.rename("out.html", "index.html")
+
+
+new_post(week_number='Six', poster_text_file='new_poster_text.txt', picture='results_week6.png')
+    
